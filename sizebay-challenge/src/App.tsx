2ab8830f44f6aa3922e7ece340a1d0131b630163
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 18:05:19 -0300

    feat: create task model interface and tasks state

--- sizebay-challenge/src/App.tsx
+++ sizebay-challenge/src/App.tsx
@@ -4,8 +4,16 @@ import { Container } from './app.ts';
 import { ProgressBar } from '@components/progressBar/index.tsx';
 import { Filter } from '@components/filter/index.tsx';
 import { Input } from '@components/input/index.tsx';
+import { useState } from 'react';
+
+interface TaskModel {
+  title: string;
+  status: 'pending' | 'done';
+}
 
 export const App = () => {
+  const [tasks, setTasks] = useState<TaskModel[]>();
+
   return (
     <Container>
       <Modal>
