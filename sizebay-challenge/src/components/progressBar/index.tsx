d1d9d94145917a736effec03916c7ab1f69cf802
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 16:04:00 -0300

    feat: create bar container stylesheet and div

--- sizebay-challenge/src/components/progressBar/index.tsx
+++ sizebay-challenge/src/components/progressBar/index.tsx
@@ -1,3 +1,18 @@
+import styled from 'styled-components';
+
+const ProgressBarContainer = styled.div`
+  width: 42.5rem;
+  height: 1.5rem;
+  background: #e4e4e4 0% 0% no-repeat padding-box;
+  border-radius: 4px;
+  opacity: 1;
+  overflow: hidden;
+
+  @media (max-width: 900px) {
+    width: 90%;
+  }
+`;
+
 export const ProgressBar = () => {
-  return <h1>Progress Bar</h1>;
+  return <ProgressBarContainer></ProgressBarContainer>;
 };
