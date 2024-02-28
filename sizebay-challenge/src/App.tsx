9918029f69aa9a281e9654d4ffffab209de1cd1d
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 17:05:23 -0300

    feat: add filter component to main app

--- sizebay-challenge/src/App.tsx
+++ sizebay-challenge/src/App.tsx
@@ -2,12 +2,15 @@ import './app.ts';
 import { Modal } from '@components/modal/index.tsx';
 import { Container } from './app.ts';
 import { ProgressBar } from '@components/progressBar/index.tsx';
+import { Filter } from '@components/filter/index.tsx';
 
 export const App = () => {
   return (
     <Container>
       <Modal>
         <ProgressBar progress={50} />
+
+        <Filter />
       </Modal>
     </Container>
   );
