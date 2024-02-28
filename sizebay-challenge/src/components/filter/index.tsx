6b5239267b324505e2e874e1e22bf01a4c54c657
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 17:31:23 -0300

    feat: create filter stylesheet and update filter components

--- sizebay-challenge/src/components/filter/index.tsx
+++ sizebay-challenge/src/components/filter/index.tsx
@@ -1,3 +1,83 @@
+import { filters } from '@utils/filters';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  width: 42.5rem;
+  min-height: 2.5rem;
+  display: flex;
+  justify-content: space-between;
+  flex-wrap: wrap-reverse;
+  align-items: center;
+  text-align: center;
+
+  @media (max-width: 900px) {
+    width: 90%;
+    gap: 0.8rem;
+  }
+`;
+
+const ButtonsContainer = styled.div`
+  display: flex;
+  gap: 0.5rem;
+
+  @media (max-width: 766px) {
+    width: 100%;
+    justify-content: right;
+  }
+`;
+
+const FilterButton = styled.button`
+  width: 4rem;
+  height: 2.0625rem;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  border-radius: 17px;
+  text-align: center;
+  font-size: 0.8rem;
+  font-family: Roboto;
+  letter-spacing: 0px;
+  color: #848484;
+
+  &:hover {
+    cursor: pointer;
+    background: #efefef;
+    border: 1px solid #4da6b3;
+    color: #4da6b3;
+  }
+`;
+
+const FilterInput = styled.input`
+  width: 28.125rem;
+  height: 2.5rem;
+  background: #ffffff 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+  padding-left: 1rem;
+  font-size: 0.8rem;
+  font-family: Roboto;
+  letter-spacing: 0px;
+  color: #848484;
+
+  background-size: 1.5rem;
+  background-image: url('search.png');
+  background-position: 98% 0.5rem;
+  background-repeat: no-repeat;
+
+  @media (max-width: 766px) {
+    width: 100%;
+  }
+`;
+
 export const Filter = () => {
-  return <h1>Filter</h1>;
+  return (
+    <Container>
+      <ButtonsContainer>
+        {filters.map((filter) => (
+          <FilterButton key={filter.value}>{filter.name}</FilterButton>
+        ))}
+      </ButtonsContainer>
+
+      <FilterInput type="text" placeholder="Search items" maxLength={50} />
+    </Container>
+  );
 };
