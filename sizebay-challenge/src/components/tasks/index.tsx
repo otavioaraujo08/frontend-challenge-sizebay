4aa32ed2a67d8d465cad1fdbd8552d59c1ea5c1c
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 18:40:17 -0300

    feat: create stylesheet and logic if tasks list value is equal to 0

--- sizebay-challenge/src/components/tasks/index.tsx
+++ sizebay-challenge/src/components/tasks/index.tsx
@@ -1,3 +1,55 @@
-export const Tasks = () => {
-  return <h1>Tasks</h1>;
+import styled from 'styled-components';
+import { TaskModel } from 'App';
+
+export const Container = styled.div``;
+
+export const TextHelperContainer = styled.div`
+  width: 42.5rem;
+  height: 3rem;
+
+  @media (max-width: 900px) {
+    width: 90%;
+  }
+`;
+
+export const TextHelper = styled.h1`
+  text-align: left;
+  font-size: 1.18rem;
+  font-family: Roboto;
+  font-weight: 400;
+  letter-spacing: 0px;
+  color: #848484;
+
+  @media (max-width: 900px) {
+    font-size: 0.875rem;
+  }
+
+  & > strong {
+    text-decoration: underline;
+    font-weight: 500;
+    cursor: pointer;
+  }
+`;
+
+interface TasksProps {
+  tasksList: TaskModel[] | [];
+}
+
+export const Tasks = ({ tasksList }: TasksProps) => {
+  return tasksList?.length ? (
+    <Container>
+      {tasksList.map((task) => {
+        const { title } = task;
+
+        return <div key={title}>{title}</div>;
+      })}
+    </Container>
+  ) : (
+    <TextHelperContainer>
+      <TextHelper>
+        There are no items marked as done.{' '}
+        <strong>Clear the filter here</strong> to see all items.
+      </TextHelper>
+    </TextHelperContainer>
+  );
 };
