018698ab498b7f4abe937ca14a7de8ddc450fd39
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-28 13:47:37 -0300

    feat: create container styled component

---
+++ sizebay-challenge/src/app.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background: #555555 0% 0% no-repeat padding-box;
+  min-height: 100vh;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
