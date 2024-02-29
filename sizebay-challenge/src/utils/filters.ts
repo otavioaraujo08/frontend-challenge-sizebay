78a1d6d725cf312b48c59594732d598dd77f1fe5
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-29 14:47:48 -0300

    feat: create filterList interface

--- sizebay-challenge/src/utils/filters.ts
+++ sizebay-challenge/src/utils/filters.ts
@@ -1,4 +1,11 @@
-export const filters = [
+import { FilterTitles } from 'App';
+
+export interface FiltersList {
+  name: 'None' | 'Done' | 'Pending';
+  value: FilterTitles;
+}
+
+export const filters: FiltersList[] = [
   {
     name: 'Done',
     value: 'done',
