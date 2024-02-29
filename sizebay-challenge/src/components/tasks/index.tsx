c4ef7821803e59af0c7233b71c2c579d98aa31ff
otavioaraujo08 <otavioaraujo490@gmail.com>
2024-02-29 15:36:14 -0300

    feat: create tasks stylesheet and add clear filter function in tasks props and tasks component childrens

--- sizebay-challenge/src/components/tasks/index.tsx
+++ sizebay-challenge/src/components/tasks/index.tsx
@@ -1,7 +1,52 @@
 import styled from 'styled-components';
 import { TaskModel } from 'App';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  width: 42.5rem;
+  max-height: 19rem;
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  gap: 1rem;
+  margin-bottom: 2rem;
+
+  @media (max-width: 900px) {
+    width: 90%;
+  }
+`;
+
+export const ContainerTask = styled.div`
+  width: 98.6%;
+  min-height: 2.5rem;
+  display: flex;
+  align-items: center;
+  text-align: center;
+  justify-content: flex-start;
+  padding-left: 0.5rem;
+  background: #f4f4f4 0% 0% no-repeat padding-box;
+  border: 1px solid #dbdbdb;
+  border-radius: 4px;
+
+  @media (max-width: 900px) {
+    width: 96%;
+  }
+`;
+
+export const TaskTitle = styled.h1`
+  max-width: 100%;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  overflow: hidden;
+  color: #848484;
+  font-size: 1.18rem;
+  font-family: Roboto;
+  font-weight: 400;
+  letter-spacing: 0px;
+
+  @media (max-width: 900px) {
+    font-size: 0.875rem;
+  }
+`;
 
 export const TextHelperContainer = styled.div`
   width: 42.5rem;
@@ -33,22 +78,28 @@ export const TextHelper = styled.h1`
 
 interface TasksProps {
   tasksList: TaskModel[] | [];
+  clearFilter: () => void;
 }
 
-export const Tasks = ({ tasksList }: TasksProps) => {
+export const Tasks = ({ tasksList, clearFilter }: TasksProps) => {
   return tasksList?.length ? (
     <Container>
       {tasksList.map((task) => {
         const { title } = task;
 
-        return <div key={title}>{title}</div>;
+        return (
+          <ContainerTask key={title}>
+            <TaskTitle>{title}</TaskTitle>
+          </ContainerTask>
+        );
       })}
     </Container>
   ) : (
     <TextHelperContainer>
       <TextHelper>
         There are no items marked as done.{' '}
-        <strong>Clear the filter here</strong> to see all items.
+        <strong onClick={clearFilter}>Clear the filter here</strong> to see all
+        items.
       </TextHelper>
     </TextHelperContainer>
   );
